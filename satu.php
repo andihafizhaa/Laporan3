2a317cf2a6502c485bead94fddf6aa6093bc9dbe
andihafizhaa <50434860+andihafizhaa@users.noreply.github.com>
2019-06-19 01:27:10 +0800

    Create satu.php

---
+++ satu.php
@@ -0,0 +1,14 @@
+<?php  
+echo "   
+<!DOCTYPE html>   
+<html>   
+<head>   
+<title>Latihan</title>   
+<body>   
+<h1>Laporan</h1>   
+<p>Laporan 3 kakas</p>   
+  
+</body>   
+</html>  
+";  
+?>
